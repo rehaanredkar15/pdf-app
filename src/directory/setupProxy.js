ba0698adc29149398519fcc3ae9f2084b9e204fa
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-08-14 23:00:34 +0530

    make it better

--- src/directory/setupProxy.js
+++ src/directory/setupProxy.js
@@ -2,6 +2,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    createProxyMiddleware(["/api", , "/otherApi"], { target: "https://pdf-gen20.herokuapp.com" })
+    createProxyMiddleware(["/api", , "/otherApi"], { target: "https://pdf-gen20.herokuapp.com/create-pdf" })
   );
 };
