4677fb25ee1c8d8726f37e40589eaf15735f46a0
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-08-14 18:09:58 +0530

    feat - Adding the front end of the app

--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { saveAs } from 'file-saver';
-import './App.css';
 
+import './App.css';
 
 class App extends Component {
   state = {
     name: '',
+    receiptId: 0,
+    price1: 0,
+    price2: 0,
   }
 
   handleChange = ({ target: { value, name }}) => this.setState({ [name]: value })
@@ -16,7 +19,7 @@ class App extends Component {
       .then(() => axios.get('fetch-pdf', { responseType: 'blob' }))
       .then((res) => {
         const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
-
+  
         saveAs(pdfBlob, 'newPdf.pdf');
       })
   }
@@ -25,11 +28,14 @@ class App extends Component {
     return (
       <div className="App">
         <input type="text" placeholder="Name" name="name" onChange={this.handleChange}/>
-        <br/>
-        <button class="btn" onClick={this.createAndDownloadPdf}>Download PDF</button>
+        <input type="number" placeholder="Receipt ID" name="receiptId" onChange={this.handleChange} />
+        <input type="number" placeholder="Price 1" name="price1" onChange={this.handleChange} />
+        <input type="number" placeholder="Price 2" name="price2" onChange={this.handleChange} />
+        <button onClick={this.createAndDownloadPdf}>Download PDF</button>
       </div>
     );
   }
 }
 
 export default App;
+ 
